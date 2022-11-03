c910bf8ae54e336227e022a0ed2f81470e70cd61
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-03 17:41:46 -0300

    refactor: Melhorei o código da page stories

--- src/routes/Stories/index.jsx
+++ src/routes/Stories/index.jsx
@@ -22,11 +22,11 @@ import img2_section5_stories from '../../assets/images/img2_section5_stories.png
 import img3_section5_stories from '../../assets/images/img3_section5_stories.png';
 import img4_section5_stories from '../../assets/images/img4_section5_stories.png';
 
-import { Container, SectionOne, CardsStories } from "./style";
+import { SectionOne, CardsStories } from "./style";
 
 export function Stories() {
   return (
-    <Container>
+    <>
       <Header />
 
       <main>
@@ -44,28 +44,28 @@ export function Stories() {
         </SectionOne>
 
         <CardsStories img={[img1_section4, img2_section4, img3_section4, img4_section4]}>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>April 16th 2020</p>
             <h3>The Mountains</h3>
             <p>by John Appleseed</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>April 14th 2020</p>
             <h3>Sunset Cityscapes</h3>
             <p>by Benjamin Cruz</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>April 11th 2020</p>
             <h3>18 Days Voyage</h3>
             <p>by Alexei Borodin</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>April 9th 2020</p>
             <h3>Architecturals</h3>
             <p>by Samantha Brooke</p>
@@ -75,28 +75,28 @@ export function Stories() {
         </CardsStories>
 
         <CardsStories img={[img1_section3_stories, img2_section3_stories, img3_section3_stories, img4_section3_stories]}>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>April 7th 2020</p>
             <h3>World Tour 2019</h3>
             <p>by Timothy Wagner</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>April 3rd 2020</p>
             <h3>Unforeseen Corners</h3>
             <p>by William Malcolm</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 29th 2020</p>
             <h3>King on Africa: Part II</h3>
             <p>by Tim Hillenburg</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 21st 2020</p>
             <h3>The Trip to Nowhere</h3>
             <p>by Felicia Rourke </p>
@@ -106,28 +106,28 @@ export function Stories() {
         </CardsStories>
 
         <CardsStories img={[img1_section4_stories, img2_section4_stories, img3_section4_stories, img4_section4_stories]}>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 19th 2020</p>
             <h3>Rage of The Sea</h3>
             <p>by Mohammed Abdul</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 16th 2020</p>
             <h3>Running Free</h3>
             <p>by Michelle</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 11th 2020</p>
             <h3>Behind the Waves</h3>
             <p>by Lamarr Wilson</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 9th 2020</p>
             <h3>Calm Waters</h3>
             <p>by Samantha Brooke</p>
@@ -137,28 +137,28 @@ export function Stories() {
         </CardsStories>
 
         <CardsStories img={[img1_section5_stories, img2_section5_stories, img3_section5_stories, img4_section5_stories]}>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 5th 2020</p>
             <h3>The Milky Way</h3>
             <p>by Benjamin Cruz</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 4th 2020</p>
             <h3>Night at The Dark Forest</h3>
             <p>by  Mohammed Abdul</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>March 1st 2020</p>
             <h3>Somwarpet’s Beauty</h3>
             <p>by Michelle</p>
             <span></span>
             <h4>READ STORY <img src={arrow_white} alt="Arrow right" /></h4>
           </div>
-          <div onClick={() => { navigate('/stories') }}>
+          <div >
             <p>February 25th 2020</p>
             <h3>Land of Dreams</h3>
             <p>by William Malcolm</p>
@@ -169,6 +169,6 @@ export function Stories() {
       </main>
 
       <Footer />
-    </Container>
+    </>
   )
 }
--- src/routes/Stories/style.jsx
+++ src/routes/Stories/style.jsx
@@ -2,10 +2,6 @@ import styled from "styled-components";
 
 import img1_Section1_stories from "../../assets/images/img1_section1_stories.png";
 
-export const Container = styled.div`
-  
-`
-
 export const SectionOne = styled.section`
   background-image: url(${img1_Section1_stories});
   background-position: center;
@@ -53,7 +49,6 @@ export const CardsStories = styled.section`
     justify-content: flex-end;
     gap: 4px;
     padding: 30px;
-
     color: white;
     z-index: 10;
     background-position: center;
@@ -61,31 +56,12 @@ export const CardsStories = styled.section`
     background-size: cover;
     position: relative;
     overflow: hidden;
-    transition: overflow 0.1s, transform 0.3s ease-out;
     cursor: pointer;
+    transition: overflow 0s, transform 0.3s ease-out;
 
-    h3{
-      margin: 0px;
-    }
-    p{
-      margin:0px;
-    }
-    span{
-      border-bottom: 1px solid white;
-      opacity: 0.25;
-      margin-top: 12px;
-      margin-bottom: 16px;
-    }
-    h4{
-      letter-spacing: 2px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin: 0px;
-
-      img{
-        width: 28px;
-      }
+    &:active, &:hover{
+      transform: translateY(-20px);
+      overflow: visible;
     }
 
     &::after{
@@ -100,22 +76,55 @@ export const CardsStories = styled.section`
       left: 0;
     }
 
-    &:active, &:hover{
-      transform: translateY(-20px);
-      overflow: visible;
+    &::before{
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: #000000 ;
+      z-index: 1;
+      opacity: 0.3;
+    }
+
+    h3, p, span, h4{
+      z-index: 2;
+    }
+
+    p{
+      font-size: 13px;
+    }
+
+    span{
+      border-bottom: 1px solid white;
+      opacity: 0.25;
+      margin-top: 12px;
+      margin-bottom: 16px;
+    }
+
+    h4{
+      letter-spacing: 2px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      img{
+        width: 28px;
+      }
     }
   }
 
   div:nth-child(1){
-    background-image: url(${ ({img}) =>img[0]});
+    background-image: url(${({ img }) => img[0]});
   }
   div:nth-child(2){
-    background-image: url(${ ({img}) =>img[1]});
+    background-image: url(${({ img }) => img[1]});
   }
   div:nth-child(3){
-    background-image: url(${ ({img}) =>img[2]});
+    background-image: url(${({ img }) => img[2]});
   }
   div:nth-child(4){
-    background-image: url(${ ({img}) =>img[3]});
+    background-image: url(${({ img }) => img[3]});
   }
 `
