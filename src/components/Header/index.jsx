73c92dcf44e97f4d1eb0769923703320ad964d1a
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-04 10:20:36 -0300

    Feat: Add responsividade ao header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Button1 } from "../../components/Buttons/style";
 
 import logo from '../../assets/icons/logo.png';
+import menu from '../../assets/icons/menu.png';
+import close from '../../assets/icons/close.png';
 
-import { Container, Nav } from "./style";
+import { Container, Nav, Logo, ContainerMenu, ImgMenu, ContainerMenuTablet } from "./style";
 
 export function Header() {
   let navigate = useNavigate()
+  const [visibleMenu, setVisibleMenu] = useState(true)
+
+  function toggleVisibleMenu() {
+    visibleMenu ? setVisibleMenu(false) : setVisibleMenu(true)
+  }
 
   return (
     <Container>
-      <div onClick={() => { navigate('/') }}>
+      <Logo onClick={() => { navigate('/') }}>
         <img src={logo} alt="Logo da photosnap" />
         <p>PHOTOSNAP</p>
-      </div>
-      <Nav>
-        <a onClick={() => { navigate('/stories') }}>STORIES</a>
-        <a onClick={() => { navigate('/features') }}>FEATURES</a>
-        <a onClick={() => { navigate('/pricing') }}>PRICING</a>
-      </Nav>
-      <Button1 onClick={() => { navigate('/pricing') }}>GET AN INVITE</Button1>
+      </Logo>
+
+      <ContainerMenu>
+        <Nav>
+          <a onClick={() => { navigate('/stories') }}>STORIES</a>
+          <a onClick={() => { navigate('/features') }}>FEATURES</a>
+          <a onClick={() => { navigate('/pricing') }}>PRICING</a>
+        </Nav>
+        <Button1 onClick={() => { navigate('/pricing') }}>GET AN INVITE</Button1>
+      </ContainerMenu>
+
+      {visibleMenu ? (
+        <ImgMenu onClick={toggleVisibleMenu} src={menu} alt="ícone do menu" />
+      ) : (
+        <ImgMenu onClick={toggleVisibleMenu} src={close} alt="ícone do menu" />
+      )}
+
+      <ContainerMenuTablet isVisible={visibleMenu}>
+        <Nav>
+          <a onClick={() => { navigate('/stories') }}>STORIES</a>
+          <a onClick={() => { navigate('/features') }}>FEATURES</a>
+          <a onClick={() => { navigate('/pricing') }}>PRICING</a>
+        </Nav>
+        <Button1 onClick={() => { navigate('/pricing') }}>GET AN INVITE</Button1>
+      </ContainerMenuTablet>
     </Container>
   )
 }
--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,30 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  display: flex;
-  justify-content: space-around;
+  display: grid;
+  grid-template: auto / 30% 70%;
+  justify-content: space-between;
+  padding: 0px 40px;
   align-items: center;
-
   color: black;
   height: 70px;
+  position: relative;
+  z-index: 15;
+`
 
+export const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+  z-index: 15;
 
-  div{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 8px;
-    cursor: pointer;
-
-    img{
-      width: 25px;
-    }
-
-    p{
-      font-weight: 700;
-      font-size: 20px;
-    } 
+  img{
+    width: 25px;
   }
+
+  p{
+    font-weight: 700;
+    font-size: 20px;
+  } 
 `
 
 export const Nav = styled.nav`
@@ -36,8 +39,71 @@ export const Nav = styled.nav`
   font-weight: 500;
   letter-spacing: 2px;
 
+  @media screen and (max-width: 768px){
+    flex-direction: column;
+    gap: 20px;
+    border-bottom: 1px solid #000;
+    padding-bottom: 20px;
+
+    a{
+      text-align: center;
+      width: 80vw;
+    }
+  }
+
   a:active, a:hover{
     opacity: 0.3;
     text-decoration: none;
+  }
+`
+
+export const ContainerMenu = styled.section`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 50px;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+`
+
+export const ImgMenu = styled.img`
+  width: 20px;
+  justify-self: flex-end;
+  z-index: 15;
+  cursor: pointer;
+
+  &:active{
+    transform: scale(0.90);
+  }
+
+  @media screen and (min-width: 769px){
+    display: none;
+  }
+`
+
+export const ContainerMenuTablet = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+
+  background-color: white;
+  padding: 32px;
+  z-index: 0;
+  top: ${({isVisible})=> isVisible ? '60px' : '-250px'};
+  left: 0;
+  right: 0;
+  position: absolute;
+  transition: all 0.2s ease-out;
+
+  button{
+    width: 80%;
+  }
+
+  @media screen and (min-width: 769px){
+    display: none;
   }
 `
